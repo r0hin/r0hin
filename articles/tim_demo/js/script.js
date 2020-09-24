34aed7fd9204d86b03f454bba8dc60158ed1b4a1
rohin12 <me@r0h.in>
2020-09-24 12:03:46 -0400

    Update

--- articles/tim_demo/js/script.js
+++ articles/tim_demo/js/script.js
@@ -4,6 +4,13 @@ function expand(el) {
     $(el).get(0).onclick = function() {
         collapse(this)
     }
+
+
+    $('#servicestext').toggleClass('hidden')
+    $('#homebtn').html('<i class="material-icons">home</i> HOME')
+    $('#gamebtn').html('<i class="material-icons">games</i> GAME SERVERS')
+    $('#contactbtn').html('<i class="material-icons">contact_page</i> CONTACT')
+    $('#loginbtn').html('<i class="material-icons">login</i> LOGIN')
 }
 
 function collapse(el) {
@@ -12,6 +19,12 @@ function collapse(el) {
     $(el).get(0).onclick = function() {
         expand(this)
     }
+
+    $('#servicestext').toggleClass('hidden')
+    $('#homebtn').html('<i class="material-icons">home</i>')
+    $('#gamebtn').html('<i class="material-icons">games</i>')
+    $('#contactbtn').html('<i class="material-icons">contact_page</i>')
+    $('#loginbtn').html('<i class="material-icons">login</i>')
 }
 
 function go(url) {
@@ -20,11 +33,13 @@ function go(url) {
     a.id = 'goEl'
     document.getElementsByTagName('body')[0].appendChild(a)
     document.getElementById('goEl').click()
-
 }
 
 function updateKeys(ey) {
     a = document.getElementsByClassName('game')
+
+    eys = ey.value.split(';')
+    console.log(eys);
 
     ey = ey.value
     if (ey == ' ' || ey == ' ') {
@@ -34,11 +49,20 @@ function updateKeys(ey) {
     }
 
     for (let i = 0; i < a.length; i++) {
-        if (!a[i].getAttribute('gameName').includes(ey)) {
+
+        val = false
+
+        for (let k = 0; k < a[i].getAttribute('gameName').split(', ').length; k++) {
+            if (a[i].getAttribute('gameName').split(', ')[k].includes(ey)) {
+                val = true
+            }
+        }
+
+        if (!val) {
             a[i].classList.add('hidden')
         }
         else {
             a[i].classList.remove('hidden')
-        }        
+        }
     }
 }
