32fd0adecaadba484dbd585d1cf0e0aebc5735be
r0hin <me@r0h.in>
2020-11-01 15:14:33 -0500

    Website redux

--- js/index.js
+++ js/index.js
@@ -3,7 +3,4 @@ function tabe(tab, el) {
     $(`.page`).addClass('hidden')
     $(`#${tab}`).removeClass('hidden')
 
-    $(`.nav_buttonactive`).removeClass('nav_buttonactive')
-    $(el).addClass('nav_buttonactive')
-
 }
