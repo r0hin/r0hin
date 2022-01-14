4fa88a9ef48d76ffed62ab6efb36f5841d36131f
Rohin <git@r0h.in>
2022-01-14 01:08:26 -0500

    Update

--- articles/paid/Randy/assets/layout.js
+++ articles/paid/Randy/assets/layout.js
@@ -2,7 +2,6 @@
 function loadHeader(activePage) {
   const a = document.createElement('div');
   a.innerHTML = `
-  
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container-fluid">
       <img class="navbar-brand" src="./assets/images/logo.webp">Project Articulate</img>
@@ -20,13 +19,11 @@ function loadHeader(activePage) {
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+              Our Impact
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a id="pastLink" class="dropdown-item" onclick="goToPage('past')">Past Action</a></li>
+              <li><a id="futureLink" class="dropdown-item" onclick="goToPage('future')">Future Plans</a></li>
             </ul>
           </li>
           <li class="nav-item">
