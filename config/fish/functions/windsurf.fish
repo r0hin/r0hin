7f2dd3fa1835a7a0c2f47633b29d4f5473379454
Rohin <git@r0h.in>
2025-02-04 13:17:54 -0500

    windsurf 😭

--- config/fish/functions/windsurf.fish
+++ config/fish/functions/windsurf.fish
@@ -1,4 +1,4 @@
-function windsurf --wraps=/Applications/Windsurf.app --wraps=/Applications/Windsurf.app/Contents/MacOS/Electron --description 'alias windsurf /Applications/Windsurf.app/Contents/MacOS/Electron'
+function windsurf --wraps=/Applications/Windsurf.app/Contents/MacOS/Electron --description 'alias windsurf /Applications/Windsurf.app/Contents/MacOS/Electron'
   /Applications/Windsurf.app/Contents/MacOS/Electron $argv
         
 end
