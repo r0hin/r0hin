5de17eeea97d63ad950e3db98c81717495c82c70
Rohin <git@r0h.in>
2026-06-17 01:01:29 -0400

    Create fix-kitty-icon.sh

---
+++ config/raycast/fix-kitty-icon.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Fix Kitty Icon
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🐱
+# @raycast.packageName Utilities
+
+# Documentation:
+# @raycast.description Re-apply the custom rose-pine kitty icon (run after a kitty update resets it)
+# @raycast.author r0hin
+# @raycast.authorURL https://raycast.com/r0hin
+
+bash "$HOME/.config/kitty/fix-icon.sh"
